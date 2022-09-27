69c2001f813f08bda419f0771b44def03ec64967
Samantha Raut <sraut@addventures.com>
2022-09-27 16:39:34 -0400

    Add changes to index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function BlockQuote() {
+  return (
+    <figure>
+      <blockquote>
+          <p></p>
+      </blockquote>
+      <figcaption>
+        <cite></cite>
+      </figcaption>
+    </figure>
+  );
+}
